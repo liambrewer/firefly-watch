3e53eebbf40de30ec501967169cfee59598bc98b
Liam Brewer <liam.a.brewer@gmail.com>
2022-08-15 23:19:17 -0500

    Mantine Setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,30 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { emotionCache } from '../utils/emotionCache';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Firefly Watch</title>
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+        emotionCache={emotionCache}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
   );
 }
 
---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+// _document.tsx
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyles, createStylesServer } from '@mantine/next';
+import { emotionCache } from '../utils/emotionCache';
+
+const stylesServer = createStylesServer(emotionCache);
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key='styles'
+        />,
+      ],
+    };
+  }
+}
