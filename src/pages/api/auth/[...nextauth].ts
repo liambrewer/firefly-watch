36f0fa5994ab88a0a1d565b887fc23c490ac46cc
Liam Brewer <liam.a.brewer@gmail.com>
2022-08-15 23:04:01 -0500

    Prisma Init with Next Auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,13 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
