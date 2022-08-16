c3a79226b84c94c888f4c571f8a08795143e4a37
Liam Brewer <liam.a.brewer@gmail.com>
2022-08-15 23:31:07 -0500

    Fix extra header import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Title } from '@mantine/core';
 import type { NextPage } from 'next';
-import Header from '../components/layout/header';
 
 const Home: NextPage = () => {
   return <Title>Home</Title>;
