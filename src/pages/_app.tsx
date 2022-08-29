c779ccc4e9592799fc19ec029d367f36b8e24c4e
Liam Brewer <liam.a.brewer@gmail.com>
2022-08-28 23:22:27 -0500

    Change default loader to bars

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,7 @@ function MyApp({
         <MantineProvider
           withGlobalStyles
           withNormalizeCSS
-          theme={{ colorScheme }}
+          theme={{ colorScheme, loader: 'bars' }}
           emotionCache={emotionCache}
         >
           <NotificationsProvider>
