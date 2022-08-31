bf14e7e6b35a52073bd725b6e7959f6075d81f24
Liam Brewer <liam.a.brewer@gmail.com>
2022-08-31 00:04:11 -0500

    Basic observation view

--- src/pages/dashboard/observations/[id].tsx
+++ src/pages/dashboard/observations/[id].tsx
@@ -1,17 +1,29 @@
-import { IconArrowLeft, IconPencilPlus } from '@tabler/icons';
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { Text } from '@mantine/core';
+import { Observation, PrismaClient } from '@prisma/client';
+import { IconArrowLeft } from '@tabler/icons';
+import type {
+  GetServerSideProps,
+  NextApiRequest,
+  NextApiResponse,
+  NextPage,
+} from 'next';
 import DashboardHeader from '../../../components/dashboard/header';
 import DashboardHeaderLink from '../../../components/dashboard/header/link';
+import getServerSession from '../../../utils/getServerSession';
 
-const Observation: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
+type Props = {
+  observation: Observation;
+};
+
+const Observation: NextPage<Props> = ({ observation }) => {
+  const date = new Date(observation.time);
 
   return (
     <>
       <DashboardHeader
-        title={`Observation: ${id}`}
+        title={`Observation ${observation.id} on ${
+          date.getMonth() + 1
+        }/${date.getDate()}/${date.getFullYear()}`}
         color='violet'
         actions={
           <DashboardHeaderLink title='Back' href='/dashboard/observations'>
@@ -19,8 +31,77 @@ const Observation: NextPage = () => {
           </DashboardHeaderLink>
         }
       />
+      <Text>
+        Latitude: {observation.latitude}, Longitude: {observation.longitude}
+      </Text>
+      <Text>
+        Amount 1: {observation.amount1}, Amount 2: {observation.amount2}, Amount
+        3: {observation.amount3}
+      </Text>
+      <Text>
+        Time: {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}
+      </Text>
+      <Text>
+        Habitat: {observation.habitat}, Mowed: {String(observation.mowed)}
+      </Text>
+      <Text>
+        Temperature: {observation.temperature}, Precipitation:{' '}
+        {observation.precipitation}, Wind: {observation.wind}, Cloud Cover:{' '}
+        {observation.cloud}, Artificial Light: {observation.light}
+      </Text>
+      <Text>
+        Pattern: {observation.pattern}, Pattern Description:{' '}
+        {observation.patternDesc}
+      </Text>
+      <Text>Notes: {observation.notes}</Text>
     </>
   );
 };
 
 export default Observation;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params as { id: string };
+
+  const session = await getServerSession(
+    context.req as NextApiRequest,
+    context.res as NextApiResponse
+  );
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const prisma = new PrismaClient();
+
+    const observation = await prisma.observation.findFirst({
+      where: {
+        id,
+        userId: session.user.id,
+      },
+    });
+
+    if (!observation) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        observation: JSON.parse(JSON.stringify(observation)),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    };
+  }
+};
